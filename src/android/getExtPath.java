e3d8f457e2bedce79d1ec9e5299443f9c01dec8a
Dante Cervantes <cegodai@gmail.com>
2016-07-07 21:26:02 -0500

    no message

--- src/android/getExtPath.java
+++ src/android/getExtPath.java
@@ -43,9 +43,6 @@ import android.renderscript.RenderScript;
 import android.renderscript.ScriptIntrinsicBlur;
 import android.util.Log;
 
-/**
- * This class echoes a string called from JavaScript.
- */
 public class getExtPath extends CordovaPlugin {
 
     private ArrayList<HashMap<String, String>> songsList = new  ArrayList<HashMap<String, String>>();
@@ -201,6 +198,8 @@ public class getExtPath extends CordovaPlugin {
         }
     }
 
+}
+
 public class Blur {
 
   private static final String TAG = "Blur";
@@ -216,40 +215,12 @@ public class Blur {
           Allocation.USAGE_SCRIPT);
       final Allocation output = Allocation.createTyped(rs, input.getType());
       final ScriptIntrinsicBlur script = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
-      script.setRadius(radius /* e.g. 3.f */);
+      script.setRadius(radius);
       script.setInput(input);
       script.forEach(output);
       output.copyTo(bitmap);
       return bitmap;
     }
-
-    // Stack Blur v1.0 from
-    // http://www.quasimondo.com/StackBlurForCanvas/StackBlurDemo.html
-    //
-    // Java Author: Mario Klingemann <mario at quasimondo.com>
-    // http://incubator.quasimondo.com
-    // created Feburary 29, 2004
-    // Android port : Yahel Bouaziz <yahel at kayenko.com>
-    // http://www.kayenko.com
-    // ported april 5th, 2012
-
-    // This is a compromise between Gaussian Blur and Box blur
-    // It creates much better looking blurs than Box Blur, but is
-    // 7x faster than my Gaussian Blur implementation.
-    //
-    // I called it Stack Blur because this describes best how this
-    // filter works internally: it creates a kind of moving stack
-    // of colors whilst scanning through the image. Thereby it
-    // just has to add one new block of color to the right side
-    // of the stack and remove the leftmost color. The remaining
-    // colors on the topmost layer of the stack are either added on
-    // or reduced by one, depending on if they are on the right or
-    // on the left side of the stack.
-    //
-    // If you are using this algorithm in your code please add
-    // the following line:
-    //
-    // Stack Blur Algorithm by Mario Klingemann <mario@quasimondo.com>
 
     Bitmap bitmap = sentBitmap.copy(sentBitmap.getConfig(), true);
 
@@ -451,5 +422,4 @@ public class Blur {
     bitmap.setPixels(pix, 0, w, 0, 0, w, h);
     return (bitmap);
   }
-
 }
