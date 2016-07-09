1799038e9fc2d058fc897bbb6822ab9e7556e3f0
Dante Cervantes <cegodai@gmail.com>
2016-07-08 20:40:57 -0500

    no message

--- src/ios/getExtPath.m
+++ src/ios/getExtPath.m
@@ -12,21 +12,28 @@
 
 - (void)coolMethod:(CDVInvokedUrlCommand*)command
 {
- 
-    __block CDVPluginResult *plresult = nil;
-    NSMutableArray *songList;
-    __block int completed = 0;
-    //songsList = [[NSMutableArray alloc] init];
+    NSArray* songList = [self getMusic];
+    
+    if (command.callbackId) {
+        CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:songList];
+        [result setKeepCallbackAsBool:NO];
+        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+    }
+}
+
+- (NSArray*)getMusic
+{
+    
+    
+    NSMutableArray *songList = [[NSMutableArray alloc] init];
     
     MPMediaQuery *everything = [[MPMediaQuery alloc] init];
     
     NSArray *itemsFromGenericQuery = [everything items];
     
-    NSUInteger selcount = [itemsFromGenericQuery count];
-
+    
     
     for (MPMediaItem *song in itemsFromGenericQuery){
-        //NSString *songTitle = [song valueForProperty: MPMediaItemPropertyTitle];
         
         
         BOOL artImageFound = NO;
@@ -42,139 +49,90 @@
             artImageFound = YES;
         }
         
+        //store image in device
+        NSArray * paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        NSString * basePath = ([paths count] > 0) ? [paths objectAtIndex:0] : nil;
+        
+        //UIImage * imageToSave = [UIImage imageNamed:@"Icon.png"];
+        NSData * binaryImageData = UIImagePNGRepresentation(artworkImage);
+        
+        int randomInt = rand() % 74;
+        NSString* str = @"VP_IMG_";
+        NSString* ext = @".png";
+        NSString* imageName = [str stringByAppendingString:[NSString stringWithFormat:@"%d",randomInt]];
+        [binaryImageData writeToFile:[basePath stringByAppendingPathComponent:[imageName stringByAppendingString:ext]] atomically:YES];
+        
         NSNumber *duration = [song valueForProperty:MPMediaItemPropertyPlaybackDuration];
         NSString *genre = [song valueForProperty:MPMediaItemPropertyGenre];
         
-        /*NSLog(@"title = %@",title);
-        NSLog(@"albumTitle = %@",albumTitle);
-        NSLog(@"artist = %@",artist);
-        NSLog(@"songurl = %@",songurl);*/
-        
+    
         AVURLAsset *songURL = [AVURLAsset URLAssetWithURL:songurl options:nil];
         
         NSArray *path = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
         
         NSString *documentDir = [path objectAtIndex:0];
-        
-        //NSLog(@"Compatible Preset for selected Song = %@", [AVAssetExportSession exportPresetsCompatibleWithAsset:songURL]);
+    
         
         AVAssetExportSession *exporter = [[AVAssetExportSession alloc] initWithAsset:songURL presetName:AVAssetExportPresetAppleM4A];
         
         exporter.outputFileType = @"com.apple.m4a-audio";
         
-        NSString *filename = [NSString stringWithFormat:@"%@.m4a",title];
+        NSString *filename = [NSString stringWithFormat:@"%@.mp3",title];
         
         NSString *outputfile = [documentDir stringByAppendingPathComponent:filename];
         
         NSURL *exportURL = [NSURL fileURLWithPath:outputfile];
         
-        exporter.outputURL  = exportURL;
+        NSURL *audioURL = exportURL;
         
-        [exporter exportAsynchronouslyWithCompletionHandler:^{
-            int exportStatus = exporter.status;
-            completed++;
-            switch (exportStatus) {
-                case AVAssetExportSessionStatusFailed:{
-                    NSError *exportError = exporter.error;
-                    NSLog(@"AVAssetExportSessionStatusFailed = %@",exportError);
-                    NSString *errmsg = [exportError description];
-                    plresult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errmsg];
-                    break;
-                }
-                case AVAssetExportSessionStatusCompleted:{
-                    
-                    NSURL *audioURL = exportURL;
-                    NSMutableDictionary *songInfo = [[NSMutableDictionary alloc] init];
-                        
-
-                    /*
-
-                    items.put("Id", thisId);
-                    items.put("Album", album);
-                    items.put("Author", author);
-                    items.put("Title", title);
-                    items.put("Genre", genero);
-                    items.put("Cover", encoded);
-                    items.put("Blur", blurred);
-                    items.put("Duration", Duration);
-                    items.put("Path", thisPath);*/
-
-                    NSLog(@"AVAssetExportSessionStatusCompleted %@",audioURL);
-                    if(title != nil) {
-                        [songInfo setObject:title forKey:@"Title"];
-                    } else {
-                        [songInfo setObject:@"No Title" forKey:@"Title"];
-                    }
-                    if(albumTitle != nil) {
-                        [songInfo setObject:albumTitle forKey:@"Album"];
-                    } else {
-                        [songInfo setObject:@"No Album" forKey:@"Album"];
-                    }
-                    if(artist !=nil) {
-                        [songInfo setObject:artist forKey:@"Author"];
-                    } else {
-                        [songInfo setObject:@"No Artist" forKey:@"Author"];
-                    }
-                    
-                    [songInfo setObject:[songurl absoluteString] forKey:@"Path"];
-                    if (artImageFound) {
-                        [songInfo setObject:[imgData base64EncodedStringWithOptions:0] forKey:@"Cover"];
-                    } else {
-                        [songInfo setObject:@"No Image" forKey:@"Cover"];
-                    }
-                    
-                    [songInfo setObject:duration forKey:@"Duration"];
-                    if (genre != nil){
-                        [songInfo setObject:genre forKey:@"Genre"];
-                    } else {
-                        [songInfo setObject:@"No Genre" forKey:@"Genre"];
-                    }
-                    
-                    [songInfo setObject:[audioURL absoluteString] forKey:@"exportedurl"];
-                    [songInfo setObject:filename forKey:@"filename"];
-                    
-                    [songList addObject:songInfo];
-                    
-                    //NSLog(@"Audio Data = %@",songsList);
-                    NSLog(@"Export Completed = %d out of Total Selected = %lu",completed,(unsigned long)selcount);
-                    if (completed == selcount) {
-                        plresult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:songList];
-                        [self.commandDelegate sendPluginResult:plresult callbackId:command.callbackId];
-                    }
-                    break;
-                }
-                case AVAssetExportSessionStatusCancelled:{
-                    NSLog(@"AVAssetExportSessionStatusCancelled");
-                    plresult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Cancelled"];
-                    break;
-                }
-                case AVAssetExportSessionStatusUnknown:{
-                    NSLog(@"AVAssetExportSessionStatusCancelled");
-                    plresult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Unknown"];
-                    break;
-                }
-                case AVAssetExportSessionStatusWaiting:{
-                    NSLog(@"AVAssetExportSessionStatusWaiting");
-                    plresult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Waiting"];
-                    break;
-                }
-                case AVAssetExportSessionStatusExporting:{
-                    NSLog(@"AVAssetExportSessionStatusExporting");
-                    plresult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Exporting"];
-                    break;
-                }
-                    
-                default:{
-                    NSLog(@"Didnt get any status");
-                    break;
-                }
-            }
-        }];
+        NSMutableDictionary *songInfo = [[NSMutableDictionary alloc] init];
         
         
-        //NSLog (@"%@", songTitle);
-        //NSLog (@"t%@", [song valueForProperty: MPMediaItemPropertyPersistentID]);
+        [songInfo setObject:[NSString stringWithFormat:@"%d",randomInt] forKey:@"Id"];
+        
+        if(title != nil) {
+            [songInfo setObject:title forKey:@"Title"];
+        } else {
+            [songInfo setObject:@"No Title" forKey:@"Title"];
+        }
+        if(albumTitle != nil) {
+            [songInfo setObject:albumTitle forKey:@"Album"];
+        } else {
+            [songInfo setObject:@"No Album" forKey:@"Album"];
+        }
+        if(artist !=nil) {
+            [songInfo setObject:artist forKey:@"Author"];
+        } else {
+            [songInfo setObject:@"No Artist" forKey:@"Author"];
+        }
+        
+        //[songInfo setObject:[songurl absoluteString] forKey:@"Path"];
+        [songInfo setObject:[audioURL absoluteString] forKey:@"Path"];
+        if (artImageFound) {
+            NSString* slash = @"/";
+            [songInfo setObject:[basePath stringByAppendingString:[slash stringByAppendingString:[imageName stringByAppendingString:ext]]] forKey:@"Cover"];
+            //[songInfo setObject:[imgData base64EncodedStringWithOptions:0] forKey:@"Cover"];
+        } else {
+            [songInfo setObject:@"No Image" forKey:@"Cover"];
+        }
+        
+        [songInfo setObject:duration forKey:@"Duration"];
+        
+        if (genre != nil){
+            [songInfo setObject:genre forKey:@"Genre"];
+        } else {
+            [songInfo setObject:@"No Genre" forKey:@"Genre"];
+        }
+        
+        //[songInfo setObject:[audioURL absoluteString] forKey:@"exportedurl"];
+        //[songInfo setObject:filename forKey:@"filename"];
+        
+        
+        [songList addObject:songInfo];
+        
     }
+    
+    return songList;
 }
 
 @end
